89e89677de6a1fc2bcd81be51e227ffd64a8c611
Ivan Skovpin <ivanskovpin@gmail.com>
2019-07-31 22:55:09 +0700

    master :: * return reponse

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('app/home.html.twig');
     }
